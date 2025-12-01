2ba034e3399925559b54e4aa2508fbd1ae3b90d3
Santiago F. M. M. <santiagofernandomurguia@gmail.com>
2025-12-01 14:50:40 -0600

    tned, need to tune the drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -22,32 +22,33 @@ public class Constants {
     // Follower constants ///
     // Consists of values from the automatic, PID, and centripetal tuners
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(14.0) // Robot's mass in kilograms. Compensates for the centripetal force
-            .forwardZeroPowerAcceleration(-45.7432) // Measures how the robot decelerates when moving forward & power is cut
+            .mass(11.0) // Robot's mass. Compensates for the centripetal force
+            .forwardZeroPowerAcceleration(-16.0) // Measures how the robot decelerates when moving forward & power is cut
             // It should be negative. Otherwise, it'll accelerate when getting closer to 0
-            .lateralZeroPowerAcceleration(-52.0536) // Measures how the robot decelerates when moving sideways & power is cut
+            .lateralZeroPowerAcceleration(-35.0) // Measures how the robot decelerates when moving sideways & power is cut
             // It should be negative. Otherwise, it'll accelerate when getting closer to 0
             .useSecondaryTranslationalPIDF(true)
             .useSecondaryHeadingPIDF(true)
             .useSecondaryDrivePIDF(true)
             .translationalPIDFCoefficients(new PIDFCoefficients(0.05, 0.0, 0.0, 0.0))
-            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.35,0.0,0.01,0.015));
+            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.35,0.0,0.01,0.015))
+            .centripetalScaling(0.0002);
 
 
     // Drivetrain constants //
     // These contain constants specific to our drivetrain type, i.e. the Mecanum
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1) // Max attainable power. Must be a number between 0 & 1
-            .rightFrontMotorName("frontRightMotor")
-            .rightRearMotorName("backRightMotor")
-            .leftRearMotorName("backLeftMotor")
-            .leftFrontMotorName("frontLeftMotor")
-            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(12.04) // The final velocity achieved by the robot after forward/backward testing
-            .yVelocity(25.77); // The final velocity achieved by the robot after lateral testing
+            .rightFrontMotorName("frontRight")
+            .rightRearMotorName("backRight")
+            .leftRearMotorName("backLeft")
+            .leftFrontMotorName("frontLeft")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .xVelocity(70.0) // The final velocity achieved by the robot after forward/backward testing
+            .yVelocity(56.0); // The final velocity achieved by the robot after lateral testing
 
 
     // Localizer constants //
@@ -56,9 +57,10 @@ public class Constants {
             .hardwareMapName("otos") // Name of the OTOS in the hardware map
             .linearUnit(DistanceUnit.INCH) // Unit used to measure
             .angleUnit(AngleUnit.RADIANS) // Unit used to measure
-            .linearScalar(1.05) // Conversion factor between OTOS reading & actual inches
-            .angularScalar(0.98) // Conversion factor between OTOS reading & actual turn
-            .offset(new SparkFunOTOS.Pose2D(-4.25,0.0,-110.0 * (Math.PI / 180)));
+            .offset(new SparkFunOTOS.Pose2D(0.0,0.0,-Math.PI / 2.0))
+            .linearScalar(1.0)
+            .angularScalar(0.995);
+
             // .offset represents the sensor's position relative to the center of the robot
 
 
